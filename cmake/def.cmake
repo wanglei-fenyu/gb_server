3cd2548bc7f7e34886a050e3fb46ba2e6392fb4b
fengyu <18730322289@163.com>
2026-05-23 19:07:29 +0800

    升级c++23

--- cmake/def.cmake
+++ cmake/def.cmake
@@ -2,10 +2,9 @@ include_guard()
 
 set_property(GLOBAL PROPERTY USE_FOLDERS ON)
 
-
 set(CMAKE_CXX_STANDARD 23)
-set(CMAKE_CXX_STANDARD_REQUIRED ON)  #防止使用c++20之下的编译器
-set(CMAKE_CXX_EXTENSIONS OFF)        #禁止使用编译器的扩展
+set(CMAKE_CXX_STANDARD_REQUIRED ON)  # 防止使用c++20之下的编译器
+set(CMAKE_CXX_EXTENSIONS OFF)        # 禁止使用编译器的扩展
 
 set(CMAKE_VERBOSE_MAKEFILE ON)
 set(CMAKE_CONFIGURATION_TYPES "debug;release") # 限定构建模式 统一跨平台大小写
@@ -14,10 +13,6 @@ set(CMAKE_POSITION_INDEPENDENT_CODE OFF)
 if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
     set(LINUX true)
 endif()
-
-
-
-
 
 # 用法：conan_link_libraries(target_name [PRIVATE|PUBLIC|INTERFACE])
 macro(conan_link_libraries target_name)
@@ -28,7 +23,7 @@ macro(conan_link_libraries target_name)
         endif()
     endforeach()
     
-    # 原有的 Conan 依赖
+    # 直接使用 target 链接（头文件和库会自动传递）
     target_link_libraries(${target_name} ${_link_type}
         gbnet::gbnet
         Boost::boost
@@ -46,15 +41,7 @@ macro(conan_link_libraries target_name)
         sol2::sol2
         cxxopts::cxxopts
     )
-    
-    # Linux 下额外链接 
-    if(LINUX)
-        target_link_options(${target_name} ${_link_type}
-       	 "LINKER:--push-state,--no-as-needed,-lquadmath,--pop-state"
-        )
-    endif()
 endmacro()
-
 
 # 环境信息
 message("CMAKE_SYSTEM_NAME: ${CMAKE_SYSTEM_NAME} CMAKE_SYSTEM_VERSION: ${CMAKE_SYSTEM_VERSION}")
@@ -65,13 +52,12 @@ message("CMAKE_CXX_COMPILER_ID: ${CMAKE_CXX_COMPILER_ID}")
 message("CMAKE_CXX_COMPILER_VERSION: ${CMAKE_CXX_COMPILER_VERSION}")
 message("CMAKE_CONFIGURATION_TYPES: ${CMAKE_CONFIGURATION_TYPES}")
 
-
-
 if("${CMAKE_GENERATOR}" MATCHES "Visual Studio")
     message("OpenType: generate VS Sln")
     add_compile_options("$<$<CXX_COMPILER_ID:MSVC>:/utf-8>")
     add_compile_options("$<$<C_COMPILER_ID:MSVC>:/utf-8>")
     add_compile_options("$<$<CXX_COMPILER_ID:MSVC>:/bigobj>")
+	add_compile_options("$<$<CXX_COMPILER_ID:MSVC>:/EHa>")
 else()
     if(NOT CMAKE_BUILD_TYPE)
         message(FATAL_ERROR "If you are using single-config generator, you should set CMAKE_BUILD_TYPE=<Debug|Release>")
@@ -80,13 +66,26 @@ endif()
 
 if(LINUX)
     add_compile_definitions(LINUX)
-    set(CMAKE_CXX_FLAGS_DEBUG "-g -O0")
-    set(CMAKE_CXX_FLAGS_RELEASE "-g -O2")
-    set(CMAKE_CXX_FLAGS_ASAN "-g -Og -fsanitize=address -fsanitize-recover=address -fno-omit-frame-pointer -fsanitize=leak")
-    string(APPEND CMAKE_CXX_FLAGS "  -pthread -fcoroutines -Wall -Wno-unused-variable -Wno-unused-but-set-variable -Wno-unused-function -Wunused-result ")
-    #set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -lquadmath")
+    
+    # ========== 根据编译器类型设置不同的标志 ==========
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+        # Clang 编译器标志
+        set(CMAKE_CXX_FLAGS_DEBUG "-g -O0")
+        set(CMAKE_CXX_FLAGS_RELEASE "-g -O2")
+        set(CMAKE_CXX_FLAGS_ASAN "-g -Og -fsanitize=address -fsanitize-recover=address -fno-omit-frame-pointer -fsanitize=leak")
+        string(APPEND CMAKE_CXX_FLAGS " -pthread -Wall -Wno-unused-variable -Wno-unused-but-set-variable -Wno-unused-function -Wunused-result -Wno-gnu-folding-constant")
+    elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+        # GCC 编译器标志
+        set(CMAKE_CXX_FLAGS_DEBUG "-g -O0")
+        set(CMAKE_CXX_FLAGS_RELEASE "-g -O2")
+        set(CMAKE_CXX_FLAGS_ASAN "-g -Og -fsanitize=address -fsanitize-recover=address -fno-omit-frame-pointer -fsanitize=leak")
+        string(APPEND CMAKE_CXX_FLAGS " -pthread -fcoroutines -Wall -Wno-unused-variable -Wno-unused-but-set-variable -Wno-unused-function -Wunused-result")
+    else()
+        message(WARNING "Unknown compiler: ${CMAKE_CXX_COMPILER_ID}")
+    endif()
+    
 elseif(WIN32)
-
+    # Windows 相关配置（暂未修改）
 else()
     message(FATAL_ERROR "unsupportted OS")
 endif()
