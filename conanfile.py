ca0f47966caebe66ed800eb1710eeb363b1a2e81
CrazyFishLeo <lei.wang13@yingxiong.com>
2026-06-02 09:53:37 +0800

    fix: use libpq/17.7 (available on conan center)

--- conanfile.py
+++ conanfile.py
@@ -27,4 +27,4 @@ class GBServer(ConanFile):
         self.requires("rapidxml/1.13")
         self.requires("sol2/3.5.0")
         self.requires("cxxopts/3.3.1")
-        self.requires("libpq/17.4")
+        self.requires("libpq/17.7")
